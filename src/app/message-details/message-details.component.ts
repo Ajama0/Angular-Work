e0de2d6bda87f5a7a0a07c22f20e90ee6aa49a3f
Ajama0 <abasjama04@hotmail.com>
2024-12-27 08:38:38 +0000

    add message details component

---
+++ src/app/message-details/message-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-details',
+  standalone: false,
+  
+  templateUrl: './message-details.component.html',
+  styleUrl: './message-details.component.css'
+})
+export class MessageDetailsComponent {
+
+}
--- src/app/my-first-comp/my-first-comp.component.ts
+++ src/app/my-first-comp/my-first-comp.component.ts
@@ -13,10 +13,23 @@ export class MyFirstCompComponent {
   email : string  = "";
   message : string = "";
   isSubmitted: boolean = false;
+  //define an iterable that allows you to iterate over based on a condition
+  //whenever a user submits a form, is submitted is true, and if so we create a new record based on 
+  //the values created by the user
+  //remember the input from the user is binded to the instance variables-data binding
+  messages: Array<any> = []
 
 
   OnSubmit():void {
     this.isSubmitted = true
+    this.messages.push(
+      {
+        "name" : this.name,
+        "email" : this.email,
+        "message": this.message
+      }
+    )
+
 
     }
 
