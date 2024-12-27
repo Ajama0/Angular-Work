de9d07d7ef595e15d116653a58a28d2c58102f3d
Ajama0 <abasjama04@hotmail.com>
2024-12-27 09:50:05 +0000

    return output from parent componet to child

--- src/app/message-details/message-details.component.ts
+++ src/app/message-details/message-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-message-details',
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './message-details.component.css'
 })
 export class MessageDetailsComponent {
+
+  @Input()
+  message : any = {};
 
 }
